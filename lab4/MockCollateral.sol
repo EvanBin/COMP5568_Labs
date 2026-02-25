36f2acfba9ad388f3e269d5876ad7296dd24942e
mzhao6320-dot <mzhao6320@gmail.com>
2026-02-25 13:45:52 +0800

    Update MockCollateral.sol

--- lab4/MockCollateral.sol
+++ lab4/MockCollateral.sol
@@ -5,12 +5,13 @@ import "./SimpleERC20.sol";
 
 contract MockCollateral is SimpleERC20 {
     constructor() SimpleERC20("Mock ETH", "mETH") {
-        // 给部署者一些初始抵押物
+
         _mint(msg.sender, 1_000_000 ether);
     }
 
-    // 方便助教发币给学生
+
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
 }
+
